5f206a6ab06ed86f73291936f33340e5b6ab4e64
Carla Iten <carlaiten@gmail.com>
2018-09-15 22:32:25 +0200

    Added redirection to Login if player is not logged in

--- alexaPlayDespacito/src/app/app.module.ts
+++ alexaPlayDespacito/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -18,7 +19,8 @@ import { ApiProvider } from '../providers/api/api';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- alexaPlayDespacito/src/pages/home/home.ts
+++ alexaPlayDespacito/src/pages/home/home.ts
@@ -11,11 +11,11 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
     var audio = new Audio('https://p.scdn.co/mp3-preview/9a712112b9a333e326ff46c93e83c4c9e17b8e80?cid=774b29d4f13844c495f206cafdad9c86');
-    audio.play();
+    //audio.play();
   }
 
   public playSong() {
     var audio = new Audio('https://p.scdn.co/mp3-preview/9a712112b9a333e326ff46c93e83c4c9e17b8e80?cid=774b29d4f13844c495f206cafdad9c86');
-    audio.play();
+    //audio.play();
   }
 }
